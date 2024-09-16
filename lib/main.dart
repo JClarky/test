f21438ea1f2f0df15499f2c961810ab5ae5d2331
JClarky <jayden260100@gmail.com>
2024-09-16 22:15:31 +1200

    try this

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'dart:developer';
 import 'dart:io';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 169, 18, 18)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 169, 18, 18)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Bluetooth connection test'),
@@ -73,37 +75,40 @@ class _MyHomePageState extends State<MyHomePage> {
       log("Bluetooth supported");
     }
 
-    // listen to scan results
-// Note: `onScanResults` only returns live scan results, i.e. during scanning. Use
-//  `scanResults` if you want live scan results *or* the results from a previous scan.
-var subscription = FlutterBluePlus.onScanResults.listen((results) {
+    startScan();
+  }
+
+  void startScan() async {
+    log("Start scan");
+    var subscription = FlutterBluePlus.onScanResults.listen(
+      (results) {
         if (results.isNotEmpty) {
-            ScanResult r = results.last; // the most recently found device
-            print('${r.device.remoteId}: "${r.advertisementData.advName}" found!');
+          ScanResult r = results.last; // the most recently found device
+          print(
+              '${r.device.remoteId}: "${r.advertisementData.advName}" found!');
         }
-    },
-    onError: (e) => print(e),
-);
+      },
+      onError: (e) => print(e),
+    );
 
-// cleanup: cancel subscription when scanning stops
-FlutterBluePlus.cancelWhenScanComplete(subscription);
+    // cleanup: cancel subscription when scanning stops
+    FlutterBluePlus.cancelWhenScanComplete(subscription);
 
-// Wait for Bluetooth enabled & permission granted
-// In your real app you should use `FlutterBluePlus.adapterState.listen` to handle all states
-FlutterBluePlus.adapterState.where((val) => val == BluetoothAdapterState.on).first;
+    // Wait for Bluetooth enabled & permission granted
+    // In your real app you should use `FlutterBluePlus.adapterState.listen` to handle all states
+    await FlutterBluePlus.adapterState
+        .where((val) => val == BluetoothAdapterState.on)
+        .first;
 
-// Start scanning w/ timeout
-// Optional: use `stopScan()` as an alternative to timeout
-FlutterBluePlus.startScan(
-  withServices:[Guid("180D")], // match any of the specified services
-  withNames:["Bluno"], // *or* any of the specified names
-  timeout: Duration(seconds:15));
+    // Start scanning w/ timeout
+    // Optional: use `stopScan()` as an alternative to timeout
+    await FlutterBluePlus.startScan(
+        withServices: [Guid("180D")], // match any of the specified services
+        withNames: ["Bluno"], // *or* any of the specified names
+        timeout: Duration(seconds: 15));
 
-// wait for scanning to stop
-FlutterBluePlus.isScanning.where((val) => val == false).first;
-
-// cancel to prevent duplicate listeners
-    subscription.cancel();
+    // wait for scanning to stop
+    await FlutterBluePlus.isScanning.where((val) => val == false).first;
   }
 
   void _incrementCounter() {
